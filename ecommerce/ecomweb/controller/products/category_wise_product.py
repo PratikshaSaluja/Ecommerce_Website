55ce6bec1375821fabe7fdffc16a753aa557454a
user <pratiksha.softprodigy@gmail.com>
2022-06-24 12:18:21 +0530

    implemented search functionality

--- ecommerce/ecomweb/controller/products/category_wise_product.py
+++ ecommerce/ecomweb/controller/products/category_wise_product.py
@@ -7,13 +7,19 @@ from ecomweb.structures.Order import *
 from ecomweb.structures.author import *
 from ecomweb.structures.banner import *
 from django.views import View
+from django.db.models import Q
 
 class Home(View):
 
 
     def get(self,request):
+        search= request.GET.get('search','')
         service1 = service.objects.all()
         author1 = author.objects.all()
         banner1 = banner.objects.values('image')
-        allProds = categories.objects.values('id','image','category_name')
-        return render(request, "home/home2.html", {'allProds': allProds, 'serv100': service1 , 'author1':author1,'banner2':banner1})
+        allProds = categories.objects.filter(category_name__icontains=search).values('id','image','category_name')
+        return render(request, "home/home2.html", {'allProds': allProds, 'serv100': service1 , 'author1':author1,'banner2':banner1,'search':search})
+    def post(self,request):
+        search= request.POST.get('search')
+        desc = Product.objects.filter(Q(product_name__icontains=search)| Q(category_id__category_name=search)).values('id','product_name', 'category_id', 'price' ,'desc', 'image')
+        return render(request, 'sub_category/sub_category.html', {'search':search,'desc':desc})
