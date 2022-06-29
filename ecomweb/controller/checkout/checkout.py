f24cf5c8c2ab327da830498421056338ec2d79fc
user <pratiksha.softprodigy@gmail.com>
2022-06-29 18:44:15 +0530

    new method for thecheckout page

--- ecomweb/controller/checkout/checkout.py
+++ ecomweb/controller/checkout/checkout.py
@@ -5,35 +5,39 @@ from ecomweb.structures.Order import *
 from ecomweb.structures.cart import *
 class CheckOut(View):
     def post(self, request):
-        try:
+        
+            print("gggggggggggggggggggggggggg")
             address_line1 = request.POST.get('address_line1')
             address_line2 = request.POST.get('address_line2')
             state = request.POST.get('state')
             pincode = request.POST.get('pincode')
             phone = request.POST.get('phone')
-            user = request.user
+            
             username1 = request.user.get_username()
-            cart =Cart(user=user,product=product)
-            products = Product.get_products_by_id(list(cart.keys()))
+            
+            user = request.user
             current_user = User.objects.get(username=username1)
-            print(address_line1,address_line2,state,pincode, phone, username1, cart, products,current_user.id)
-            for product in products:
-                print(cart.get(str(product.id)))
-                order = Order(user_id=current_user.id,
-                            product=product,
-                            price=product.price,
+            product = Cart.objects.filter(user=user).values('product_id')
+       
+            print(product)
+            print(product[0]["product_id"],15)
+
+            
+            print(address_line1,address_line2,state,pincode, phone, username1, product,current_user.id)
+          
+            order = Order(user_id=current_user.id,
+                            
                             address_line1=address_line1,
                             address_line2=address_line2,
                             state=state,
                             pincode=pincode,
+                            product_id=product[0]["product_id"],
                             phone=phone,
                             )
-                order.save()
+            order.save()
             
             return redirect('/order')
-        except Exception as E:
-            print(E)
-            return render(request,'checkout/checkout.html')
+        
     def get(self,request):
         try:
             return render(request,'checkout/checkout.html')
--- ecomweb/controller/order/order.py
+++ ecomweb/controller/order/order.py
@@ -5,10 +5,8 @@ from django.views import View
 class OrderView(View):
     def get(self , request ):
         try:
-            user1 = request.session.get('username')
-            user = User.objects.get(username=user1)
-            print(user1)
-            orders = Order.get_orders_by_user(user.id)
+            
+            orders = Order.objects.values("product__product_name","date","status")
             print(orders)
             return render(request, 'orders/order.html', {'orders': orders})
         except Exception as E:
