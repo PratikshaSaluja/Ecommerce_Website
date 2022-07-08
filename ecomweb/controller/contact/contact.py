3cc6c98b9a7cb8e67fd89f37a89438de236d56c4
user <pratiksha.softprodigy@gmail.com>
2022-07-08 11:51:53 +0530

    implemented serailizers in contact page

--- ecomweb/controller/contact/contact.py
+++ ecomweb/controller/contact/contact.py
@@ -1,22 +1,18 @@
 from django.shortcuts import render, redirect
 from ecomweb.structures.contact import *
 from ecomweb.serializers import contactSerializer
-def contact(request):
-    try:
-        if request.method == "POST":
-            name = request.POST['name']
-            email = request.POST['email']
-            subject = request.POST['subject']
-            msg = request.POST['msg']
-            Contact = contacts(name=name, email=email, subject=subject, msg=msg)
-            serializer = contactSerializer(Contact)
-            data=serializer.data
-            print(serializer.data,"_________________________") 
-            serializer.is_valid()
-            serializer.save(data=data)
-            return redirect("/contact")
-        else:
+from django.views import View
+class contact(View):
+    def post(self,request):
+        try:
+            data=request.POST
+            serializer = contactSerializer(data=data)
+            if serializer.is_valid():
+                serializer.save()
+                return redirect("/contact")
+            else:
+                return render(request, "contact/contact.html")
+        except Exception as E:
             return render(request, "contact/contact.html")
-    except Exception as E:
-        print(E)
-        return render(request, "contact/contact.html")
+    def get(self,request):
+         return render(request, "contact/contact.html")
--- ecomweb/serializers.py
+++ ecomweb/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
-class contactSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=255)
-    email = serializers.CharField(max_length=100)
-    subject = serializers.CharField(max_length=1000)
-    msg = serializers.CharField(max_length=1000)
+from ecomweb.structures.contact import *
+class contactSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = contacts
+        fields = ['sno', 'name','email','subject', 'msg']
